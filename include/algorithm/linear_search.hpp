b4d9168a3255db817ba718e1d9189823888e7027
Masahisa Konno <masahisa.konno.dev@gmail.com>
2020-01-16 00:01:08 +0900

    Add linear_search algorithm

---
+++ include/algorithm/linear_search.hpp
@@ -0,0 +1,32 @@
+#ifndef RTW_LINEAR_SEARCH
+#define RTW_LINEAR_SEARCH
+
+namespace rtw {
+
+template<typename InputIterator, typename T>
+constexpr InputIterator linear_search(InputIterator first, InputIterator last, const T& value)
+{
+    while(first != last){
+        if(*first == value){
+            return first;
+        }
+        ++first;
+    }
+    return last;
+}
+
+template<typename InputIterator, typename Predicate>
+constexpr InputIterator linear_search_if(InputIterator first, InputIterator last, Predicate predicate)
+{
+    while(first != last){
+        if(predicate(*first)){
+            return first;
+        }
+        ++first;
+    }
+    return last;
+}
+
+} // namespace rtw
+
+#endif // RTW_LINEAR_SEARCH
